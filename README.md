268823fa24977ee3cada44b50b2b273e0b7233de
Yevhenii Kulish <s006644@vnmu.edu.ua>
2023-10-21 20:20:48 +0300

    meseum landing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is a landing page of the NAMU. There're a header with a menu, a main title, a logo, a link and language selector; a main block with some sections presenting events, exhibitions and news. The page also has a form with an email input. A footer of the landing page contains, opening hours, social media links, contacts, a navigation and copyrights.
 
-I used such technologies as HTML, CSS, Sass, BEM to create this landing page.
+I used such technologies as HTML, CSS, Sass, BEM, JS, DOM to create this landing page.
 
 Figma layout:
     - [Figma](https://www.figma.com/file/HL3XGt5ZatvJoYBhOaWY5x/museum-prototype?node-id=323%3A1957&mode=dev)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,40 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page--with-menu');
   }
 });
+
+const languageSelector = document.querySelector('.top__language-container');
+
+const handleLanguageSelector = (event) => {
+  event.preventDefault();
+
+  const list = languageSelector.querySelector('ul');
+  const selectorButton = languageSelector.querySelector('.top__language');
+
+  list.style.display = 'block';
+
+  const chooseLanguage = (e) => {
+    const chosenLanguage = e.target;
+
+    e.stopPropagation();
+
+    languageSelector.firstElementChild.firstElementChild
+      .textContent = chosenLanguage.textContent.trim();
+
+    list.style.display = 'none';
+    list.removeEventListener('click', chooseLanguage);
+    selectorButton.removeEventListener('click', closeSelector);
+  };
+
+  const closeSelector = (e) => {
+    e.stopPropagation();
+
+    list.style.display = 'none';
+
+    selectorButton.removeEventListener('click', closeSelector);
+  };
+
+  list.addEventListener('click', chooseLanguage);
+  selectorButton.addEventListener('click', closeSelector);
+};
+
+languageSelector.addEventListener('click', handleLanguageSelector);
